9291894ba454a548bfcb8cafe9d4271fe149f430
imkartikahuja <kartik.ahuja24@gmail.com>
2018-03-16 10:36:54 +0530

    Add logging request middleware

--- server/server.js
+++ server/server.js
@@ -2,6 +2,15 @@ const express = require('express');
 
 var app = express();
 
+//for logging request
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  next();
+});
+
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
 });

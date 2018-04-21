f08c3db31ca15f5b599506ff6d7345b111b6c1ff
imkartikahuja <kartik.ahuja24@gmail.com>
2018-04-21 18:44:47 +0530

    compile cpp 3

--- server/compiler/cpp.js
+++ server/compiler/cpp.js
@@ -18,6 +18,7 @@ const compileCpp = (code,time_limit,userId) => {
       console.error(`exec error: ${error}`);
       return;
     }
+    console.log(`${userId} folder created`);
 
     fs.writeFile(`./tmp/${userId}/code.cpp`, code, (err) => {
       if (err) {console.log(err);throw err;}
@@ -33,7 +34,7 @@ const compileCpp = (code,time_limit,userId) => {
         }
 
         console.time('execute');
-        exec(`tmp/${userId}/./a.out < tmp/${userId}/in.txt`, defaults,(error, stdout, stderr) => {
+        exec(`tmp/${userId}/./a.out < compiler/in.txt`, defaults,(error, stdout, stderr) => {
           if (error) {
             if(error.code == 139){
               console.log('Segmentation fault');
@@ -49,19 +50,28 @@ const compileCpp = (code,time_limit,userId) => {
           console.log(`stderr: ${stderr}`);
           console.timeEnd('execute');
 
-          exec(`cmp tmp/${userId}/output.txt tmp/${userId}/out.txt`, (error, stdout, stderr) => {
+          exec(`cmp tmp/${userId}/output.txt compiler/out.txt`, (error, stdout, stderr) => {
             if (error) {
               console.error(`Wrong Answer`);
               return;
             }
 
             console.log('Correct Answer');
+
+            exec(`rm -rf ./tmp/${userId}`,(error, stdout, stderr) => {
+              if (error) {
+                console.error(`exec error: ${error}`);
+                return;
+              }
+              console.log(`${userId} folder deleted`);
+            });
           });
 
         });
 
       });
     });
+
   });
 
 
